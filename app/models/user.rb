ee926682dde879bd87755780a48f4b4db94199fc
Bonnie Kim <bonniekim108@MacBook-Pro.local>
2015-01-19 17:13:42 -0800

    Add Application Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
-  
+
   field :username, type: String
   field :email, type: String
+  
   field :password_digest, type: String
 
   has_secure_password
