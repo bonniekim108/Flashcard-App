2f8f581a7e3b5a43254ff3f2f22bfa20f6daaf7b
Bonnie <bonniekim108@yahoo.com>
2015-01-25 13:58:27 -0800

    Edit Wordnik Controller Create Method

--- app/controllers/wordnik_controller.rb
+++ app/controllers/wordnik_controller.rb
@@ -6,14 +6,10 @@ class WordnikController < ApplicationController
     @word_related_words  = Wordnik.get_related_words(params[:word])
   end
 
-  def new
-    
-  end
-
   def create
-    #@word_definitions = Flashcard.new(params[:word_definitions])
-    #@word_pronunciations = Flashcard.new(params[:word_pronunciations])
-    #@word_related_words = Flashcard.new(params[:word_related_words])
+    @word_definitions    = Wordnik.get_definitions(params[:word])
+    @word_pronunciations = Wordnik.get_pronunciations(params[:word])
+    @word_related_words  = Wordnik.get_related_words(params[:word])
 
     if @flashcard.save
       redirect_to @flashcard, notice: 'Flashcard was successfully created.'
@@ -21,7 +17,6 @@ class WordnikController < ApplicationController
       render :new
     end
   end
-end
 
 
-#save @flashcard somewhere here
+
