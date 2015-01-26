9168ced1d73956c14f117edc56cf1dafff5e43fb
Bonnie <bonniekim108@yahoo.com>
2015-01-25 20:42:08 -0800

    Edit index page in Wordnik Folder

--- app/models/wordnik.rb
+++ app/models/wordnik.rb
@@ -10,21 +10,20 @@ class Wordnik
   query = {api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5", limit: 3, sourceDictionaries: "ahd"} 
     response = HTTParty.get "http://api.wordnik.com:80/v4/word.json/#{word}/definitions?includeRelated=true&useCanonical=false&includeTags=false", query: query
     return response
-    if response.save
-      redirect_to flashcards_path
   end
 
-  # def self.get_pronunciations(word)
-  #   query = {api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5", limit: 1} # sourceDictionary: "ahd"
-  #   response = HTTParty.get "http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false", query: query
-  #   return response
-  # end
+   def self.get_pronunciations(word)
+     query = {api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5", limit: 1} # sourceDictionary: "ahd"
+     response = HTTParty.get "http://api.wordnik.com:80/v4/word.json/#{word}/pronunciations?useCanonical=false", query: query
+     return response
+   end
 
-  # def self.get_related_words(word)
-  #   query = {api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5", limitPerRelationshipType: 2, relationshipTypes: "synonym", sourceDictionaries: "ahd"}
-  #   response = HTTParty.get "http://api.wordnik.com:80/v4/word.json/#{word}/relatedWords?useCanonical=false", query: query
-  #   return response
-  # end
+   def self.get_related_words(word)
+     query = {api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5", limitPerRelationshipType: 2, relationshipTypes: "synonym"}
+     response = HTTParty.get "http://api.wordnik.com:80/v4/word.json/#{word}/relatedWords?useCanonical=false", query: query
+     return response
+   end
+ end
 
   #  def self.get_related_words(word)
   #   query = {api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5", limitPerRelationshipType: 2, relationshipTypes: "antonym", sourceDictionaries: "ahd"}
@@ -32,5 +31,5 @@ class Wordnik
   #   return response
   # end
 
-end
+
 
