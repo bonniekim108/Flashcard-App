fe3a9df17d1bf390b9ed0e77ea44a036c4eaf226
Bonnie <bonniekim108@yahoo.com>
2015-03-13 17:45:01 -0700

    Got rid of unnecessary spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,13 @@
 class User
+
   include Mongoid::Document
   include ActiveModel::SecurePassword
-
   has_many :flashcards
-
   field :username, type: String
   field :email, type: String
-  
   field :password_digest, type: String
-
   has_secure_password
   validates :password, length: { minimum: 6}
-
   validates :username, presence: true, length: { maximum: 15 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-
 end
