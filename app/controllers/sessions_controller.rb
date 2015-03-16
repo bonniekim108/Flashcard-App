51dc23d90e795aeef67199a2d3688e80276287c2
Bonnie <bonniekim108@yahoo.com>
2015-03-16 13:39:12 -0700

    Tweaks to authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,6 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:user][:email])
 
 		if user && user.authenticate(params[:user][:password])
-			flash[:info] = "You logged in successfully!"
 			session[:user_id] = user.id.to_s 
 			redirect_to flashcards_path
 		else
